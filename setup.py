720be792bc53d2e9e34e5b3cf616dc55e6170801
Patrick Westphal <patrick.westphal@hitec-hamburg.de>
2023-10-13 13:14:43 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='primekgtordf',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Patrick Westphal',
+    author_email='patrick.westphal@uni-hamburg.de',
+    description='',
+    install_requires=[
+        'rdflib==7.0.0',
+    ]
+)
